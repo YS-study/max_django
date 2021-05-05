94e5861b4565dc87ebda986d3b881c3c75214ffb
이다은 <zerozero7bang@gmail.com>
2021-05-05 18:06:50 +0900

    admin 등록

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post, Question, Answer
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Question)
+admin.site.register(Answer)
