ce8cc23fdab9766c9ac9f59733d616a401ecff20
sana0803 <sana0803@naver.com>
2021-05-05 23:50:13 +0900

    sanha machine

---
+++ machines/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Machine
+
+class MachineForm(forms.ModelForm):
+
+    PICK_USED = False
+    PICK_NEW = True
+
+    PICKS = [
+        (PICK_USED, 'USED'),
+        (PICK_NEW, 'NEW'),
+    ]
+
+    pick = forms.ChoiceField(choices=PICKS)
+    class Meta:
+        model = Machine
+        fields = ('title', 'content',)
+
+
+
+
