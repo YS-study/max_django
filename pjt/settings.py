969926e3a76bae81f4afa53c86f2ef7f10abe2dc
sana0803 <sana0803@naver.com>
2021-05-08 23:56:41 +0900

    sanha machine image

--- pjt/settings.py
+++ pjt/settings.py
@@ -140,3 +140,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# 이미지 파일 경로 추가
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
