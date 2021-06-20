9f4257c2c50a479afcbfcb48b4c5f950723de8e7
sana0803 <sana0803@naver.com>
2021-06-21 05:16:04 +0900

    fixed main,machine index layout

--- board/models.py
+++ board/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -18,7 +18,7 @@ class Question(models.Model):
     name = models.CharField(max_length=10)
     password = models.CharField(max_length=20)
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
